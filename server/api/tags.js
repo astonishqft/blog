9ea470a9432bcbf86149181a1394f0e6011a8bb0
=qifutao <1551601581@qq.com>
2018-09-14 18:29:51 +0800

    新增“添加标签”接口

--- server/api/tags.js
+++ server/api/tags.js
@@ -8,7 +8,23 @@ const router = express.Router();
 // 添加标签
 router.post('/addTag', function(req, res) {
     let { name } = req.body;
-    console.log('我回来啦！！！', name)
+    console.log('新增标签:', name);
+    Tag.findOne({name}).then(result => {
+        if(!result) {
+            let tag = new Tag({
+                name
+            });
+            tag.save().then(data => {
+                responseClient(res, 200, 0, '标签添加成功', data);
+            }).catch(err => {
+                throw err;
+            })
+        }else {
+            responseClient(res, 200, 1, '该标签已存在');
+        }
+    }).catch(err => {
+        responseClient(res);
+    })
 });
 
 module.exports = router;
