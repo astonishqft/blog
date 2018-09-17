ae7e4f2a4dc195f3a8f575d7d0b974cd05a3b749
=qifutao <1551601581@qq.com>
2018-09-17 19:00:35 +0800

    增加webpack配置

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import express from 'express';
 import favicon from 'serve-favicon';
 import httpProxy from 'http-proxy';
 import compression from 'compression';
-// import history from 'connect-history-api-fallback';
+import history from 'connect-history-api-fallback';
 import config from '../config/config';
 
 const app = new express();
@@ -15,6 +15,8 @@ const proxy = httpProxy.createProxyServer({
     target: targetUrl
 });
 
+app.use('/', history());
+
 app.use('/api', (req, res) => {
     proxy.web(req, res, { target: targetUrl});
 })
@@ -23,6 +25,28 @@ app.use(compression());
 app.use('/', express.static(path.join(__dirname, '..', 'build')));
 
 app.use(favicon(path.join(__dirname, '..', 'public', 'favicon.ico')));
+
+//热更新
+if(process.env.NODE_ENV !== 'production') {
+    const Webpack = require('webpack');
+    const WebpackDevMiddleware = require('webpack-dev-middleware');
+    const WebpackHotMiddleware = require('webpack-hot-middleware');
+    const webpackConfig = require('../webpack.dev');
+
+    const compiler = Webpack(webpackConfig);
+
+    app.use(WebpackDevMiddleware(compiler, {
+        publicPath: '/',
+        stats: {colors: true},
+        lazy: false,
+        watchOptions: {
+            aggregateTimeout: 300,
+            poll: true
+        },
+    }));
+
+    app.use(WebpackHotMiddleware(compiler));
+}
 
 app.listen(port, err => {
     if(err) {
---
+++ webpack.dev.js
@@ -0,0 +1,113 @@
+const pathLib = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const OpenBrowserPlugin = require('open-browser-webpack-plugin');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+const CleanPlugin = require('clean-webpack-plugin');
+const config = require('./config/config');
+
+const ROOT_PATH = pathLib.resolve(__dirname);
+const ENTRY_PATH = pathLib.resolve(ROOT_PATH, 'src');
+const OUTPUT_PATH = pathLib.resolve(ROOT_PATH, 'build');
+const AppHtml = pathLib.resolve(ENTRY_PATH, 'index.html');
+
+module.exports = {
+    entry: {
+        index: [
+            'react-hot-loader/patch',
+            `webpack-hot-middleware/client?path=http://${config.host}:${config.port}/__webpack_hmr`,
+            'babel-polyfill',
+            pathLib.resolve(ENTRY_PATH, 'index.js')
+        ],
+        vendor: ['react', 'react-dom', 'react-router-dom']
+    },
+    output: {
+        path: OUTPUT_PATH,
+        publicPath: "/",
+        filename: "[name]-[hash:9].js",
+    },
+    devtool: "cheap-module-eval-source-map",
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: ['style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            localIdentName: '[name]-[local]-[hash:base64:5]',
+                            importLoaders: 1
+                        }
+                    },
+                    'postcss-loader'
+                ]
+            },
+            {
+                test: /\.css$/,
+                include: /node_modules/,
+                use: ['style-loader',
+                    {
+                        loader: 'css-loader'
+                    },
+                    'postcss-loader'
+                ]
+            },
+            {
+                test: /\.less$/,
+                use: ["style-loader", 'css-loader', "postcss-loader", "less-loader"]
+            },
+            {
+                test: /\.(png|jpg|gif|JPG|GIF|PNG|BMP|bmp|JPEG|jpeg)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(eot|woff|ttf|woff2|svg)$/,
+                use: 'url-loader'
+            }
+        ]
+    },
+    plugins: [
+
+        new ProgressBarPlugin(),
+        new webpack.optimize.AggressiveMergingPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.DefinePlugin({
+            "progress.env.NODE_ENV": JSON.stringify('development')
+        }),
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: AppHtml,
+        }),
+        new webpack.NoEmitOnErrorsPlugin(),
+        new webpack.HashedModuleIdsPlugin(),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            minChunks: function (module) {
+                return module.context && module.context.indexOf('node_modules') !== -1;
+            }
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: "manifest"
+        })
+
+    ],
+    resolve: {
+        extensions: ['.js', '.json', '.sass', '.scss', '.less', 'jsx']
+    }
+};
+
